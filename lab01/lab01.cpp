7ce984272eda7f67001e7a72fd8df2984b279942
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-09-18 19:20:07 -0300

    change from vector to list on lab01

--- lab01/lab01.cpp
+++ lab01/lab01.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <list>
 #include <stdlib.h>
 using namespace std;
 
@@ -9,19 +9,19 @@ class Central;
 class Central {
     public:
         string origin;
-        vector<Central*> destinations;
+        list<Central*> destinations;
 
         bool visited;
         bool isPresentInTheOther;
         Central():isPresentInTheOther(false) {};
 };
 
-vector<Central*> buildGraph();
-bool areFromSameCity(vector<Central*> oldBlueprint, vector<Central*> newBlueprint);
+list<Central*> buildGraph();
+bool areFromSameCity(list<Central*> oldBlueprint, list<Central*> newBlueprint);
 
 int main() {
-    vector<Central*> oldBlueprint = buildGraph();
-    vector<Central*> newBlueprint = buildGraph();
+    list<Central*> oldBlueprint = buildGraph();
+    list<Central*> newBlueprint = buildGraph();
 
     bool yes = areFromSameCity(oldBlueprint, newBlueprint);
     if(yes) {
@@ -33,13 +33,13 @@ int main() {
     return 0;
 }
 
-void remove(Central* font, Central* destiny, vector<Central*> oldBlueprint) {
-    for (vector<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
+void remove(Central* font, Central* destiny, list<Central*> oldBlueprint) {
+    for (list<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
         Central* central = *it;
         if(central->origin.compare(font->origin) == 0 ||
             central->origin.compare(destiny->origin) == 0) {
             
-            for (vector<Central*>::iterator it2 = central->destinations.begin(); it2 != central->destinations.end(); it2++) {
+            for (list<Central*>::iterator it2 = central->destinations.begin(); it2 != central->destinations.end(); it2++) {
                 Central* central2 = *it2;
                 if(central2->origin.compare(font->origin) == 0 ||
                         central2->origin.compare(destiny->origin) == 0) {
@@ -50,10 +50,10 @@ void remove(Central* font, Central* destiny, vector<Central*> oldBlueprint) {
     }
 }
 
-void depthSearchVisit(Central* font, Central* vertex, vector<Central*> oldBlueprint) {
+void depthSearchVisit(Central* font, Central* vertex, list<Central*> oldBlueprint) {
     vertex->visited = true;
 
-for (vector<Central*>::iterator it = vertex->destinations.begin(); it != vertex->destinations.end(); it++) {
+for (list<Central*>::iterator it = vertex->destinations.begin(); it != vertex->destinations.end(); it++) {
         Central* adjacentVertex = *it;
         if(!adjacentVertex->visited) {
             if(adjacentVertex->isPresentInTheOther) {
@@ -65,11 +65,11 @@ for (vector<Central*>::iterator it = vertex->destinations.begin(); it != vertex-
     }
 }
 
-bool checkVertexes(vector<Central*> oldBlueprint, vector<Central*> newBlueprint) {
-    for (vector<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
+bool checkVertexes(list<Central*> oldBlueprint, list<Central*> newBlueprint) {
+    for (list<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
         Central* oldCentral = *it;
         bool found = false;
-        for (vector<Central*>::iterator it2 = newBlueprint.begin(); it2 != newBlueprint.end(); it2++) {
+        for (list<Central*>::iterator it2 = newBlueprint.begin(); it2 != newBlueprint.end(); it2++) {
             Central* newCentral = *it2;
             if(newCentral->origin.compare(oldCentral->origin) == 0) {
                 newCentral->isPresentInTheOther = true;
@@ -84,13 +84,13 @@ bool checkVertexes(vector<Central*> oldBlueprint, vector<Central*> newBlueprint)
     return true;
 }
 
-bool areFromSameCity(vector<Central*> oldBlueprint, vector<Central*> newBlueprint) {
+bool areFromSameCity(list<Central*> oldBlueprint, list<Central*> newBlueprint) {
     checkVertexes(oldBlueprint, newBlueprint);
 
-    for (vector<Central*>::iterator it = newBlueprint.begin(); it != newBlueprint.end(); it++) {
+    for (list<Central*>::iterator it = newBlueprint.begin(); it != newBlueprint.end(); it++) {
         Central* vertex = *it;
 
-        for (vector<Central*>::iterator it2 = newBlueprint.begin(); it2 != newBlueprint.end(); it2++) {
+        for (list<Central*>::iterator it2 = newBlueprint.begin(); it2 != newBlueprint.end(); it2++) {
                 Central* v = *it2;
                 v->visited = false;
         }
@@ -99,7 +99,7 @@ bool areFromSameCity(vector<Central*> oldBlueprint, vector<Central*> newBlueprin
         }
     }
 
-    for (vector<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
+    for (list<Central*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end(); it++) {
         Central* vertex = *it;
         if(vertex->destinations.size() > 0) {
             return false;
@@ -109,8 +109,8 @@ bool areFromSameCity(vector<Central*> oldBlueprint, vector<Central*> newBlueprin
     return true;
 }
 
-vector<Central*> buildGraph() {
-    vector<Central*> graph;
+list<Central*> buildGraph() {
+    list<Central*> graph;
     
     unsigned int m1;
     cin >> m1;
@@ -120,7 +120,7 @@ vector<Central*> buildGraph() {
         cin >> vertex1 >> vertex2;
 
         Central *central1 = NULL, *central2 = NULL;
-        for (vector<Central*>::iterator it = graph.begin(); it != graph.end(); it++) {
+        for (list<Central*>::iterator it = graph.begin(); it != graph.end(); it++) {
             Central* central = *it;
 
             if(central->origin.compare(vertex1) == 0) {
