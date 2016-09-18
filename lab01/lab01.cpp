2ffa9106f308ffe26b102fa9d59331b265d86d3a
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-09-18 19:44:31 -0300

    rename to better names

--- lab01/lab01.cpp
+++ lab01/lab01.cpp
@@ -8,8 +8,8 @@ typedef enum { WHITE, GREY, BLACK } Color;
 class Vertex;
 class Vertex {
     public:
-        string origin;
-        list<Vertex*> destinations;
+        string name;
+        list<Vertex*> adjacents;
 
         bool visited;
 };
@@ -34,29 +34,28 @@ int main() {
 Vertex* find(list<Vertex*> graph, string searched) {
     for (list<Vertex*>::iterator it = graph.begin(); it != graph.end();  it++) {
         Vertex* vertex = *it;
-        if(searched.compare(vertex->origin) == 0) {
+        if(searched.compare(vertex->name) == 0) {
             return vertex;
         }
     }
     return NULL;
 }
 
-bool isThereAPathWithOnlyNewCentrals(Vertex* vertex, string destiny, 
+bool isThereAPathWithOnlyNewVertexes(Vertex* vertex, string destiny, 
                                                 list<Vertex*> oldBlueprint) {
     vertex->visited = true;
-    for (list<Vertex*>::iterator it = vertex->destinations.begin(); it != vertex->destinations.end();  it++) {
+    for (list<Vertex*>::iterator it = vertex->adjacents.begin(); it != vertex->adjacents.end();  it++) {
         Vertex* adjacentVertex = *it;
         if(!adjacentVertex->visited) {
-            
-            if(adjacentVertex->origin.compare(destiny) == 0) {
+            if(adjacentVertex->name.compare(destiny) == 0) {
                 return true;
             }
 
-            if(find(oldBlueprint, adjacentVertex->origin)) {
+            if(find(oldBlueprint, adjacentVertex->name)) {
                 continue;
             }
 
-            bool result = isThereAPathWithOnlyNewCentrals(adjacentVertex, destiny, oldBlueprint);
+            bool result = isThereAPathWithOnlyNewVertexes(adjacentVertex, destiny, oldBlueprint);
             if(result) {
                 return true;
             }
@@ -67,22 +66,22 @@ bool isThereAPathWithOnlyNewCentrals(Vertex* vertex, string destiny,
 
 bool areFromSameCity(list<Vertex*> oldBlueprint, list<Vertex*> newBlueprint) {
     for (list<Vertex*>::iterator it = oldBlueprint.begin(); it != oldBlueprint.end();  it++) {
-        Vertex* oldOrigin = *it;
-        Vertex* newOrigin = find(newBlueprint, oldOrigin->origin);
+        Vertex* oldVertexFont = *it;
+        Vertex* newVertexFont = find(newBlueprint, oldVertexFont->name);
 
-        if(newOrigin == NULL) {
+        if(newVertexFont == NULL) {
             return false;
         }
 
-        for (list<Vertex*>::iterator it2 = oldOrigin->destinations.begin(); it2 != oldOrigin->destinations.end();  it2++) {
-            Vertex* oldDestiny = *it2;
+        for (list<Vertex*>::iterator it2 = oldVertexFont->adjacents.begin(); it2 != oldVertexFont->adjacents.end();  it2++) {
+            Vertex* oldVertexDestiny = *it2;
 
             for (list<Vertex*>::iterator it3 = newBlueprint.begin(); it3 != newBlueprint.end();  it3++) {
                 Vertex* v = *it3;
                 v->visited = false;
             }
-            bool isCorrectPathThere = isThereAPathWithOnlyNewCentrals(newOrigin, 
-                                        oldDestiny->origin, oldBlueprint);
+            bool isCorrectPathThere = isThereAPathWithOnlyNewVertexes(newVertexFont, 
+                                        oldVertexDestiny->name, oldBlueprint);
             if(!isCorrectPathThere) {
                 return false;
             }
@@ -101,37 +100,37 @@ list<Vertex*> buildGraph() {
         string vertex1, vertex2;
         cin >> vertex1 >> vertex2;
 
-        Vertex *central1 = NULL, *central2 = NULL;
+        Vertex *vertexFont = NULL, *vertexDestiny = NULL;
         for (list<Vertex*>::iterator it = graph.begin(); it != graph.end();  it++) {
-            Vertex* central = *it;
+            Vertex* vertex = *it;
 
-            if(central->origin.compare(vertex1) == 0) {
-                central1 = central;
+            if(vertex->name.compare(vertex1) == 0) {
+                vertexFont = vertex;
             }
 
-            if(central->origin.compare(vertex2) == 0) {
-                central2 = central;
+            if(vertex->name.compare(vertex2) == 0) {
+                vertexDestiny = vertex;
             }
 
-            if(central1 != NULL && central2 != NULL) {
+            if(vertexFont != NULL && vertexDestiny != NULL) {
                 break;
             }
         }
 
-        if(central1 == NULL) {
-            central1 = new Vertex();
-            central1->origin = vertex1;
-            graph.push_back(central1);
+        if(vertexFont == NULL) {
+            vertexFont = new Vertex();
+            vertexFont->name = vertex1;
+            graph.push_back(vertexFont);
         }
 
-        if(central2 == NULL) {
-            central2 = new Vertex();
-            central2->origin = vertex2;
-            graph.push_back(central2);
+        if(vertexDestiny == NULL) {
+            vertexDestiny = new Vertex();
+            vertexDestiny->name = vertex2;
+            graph.push_back(vertexDestiny);
         }
 
-        central1->destinations.push_back(central2);
-        central2->destinations.push_back(central1);
+        vertexFont->adjacents.push_back(vertexDestiny);
+        vertexDestiny->adjacents.push_back(vertexFont);
     }
 
     return graph;
